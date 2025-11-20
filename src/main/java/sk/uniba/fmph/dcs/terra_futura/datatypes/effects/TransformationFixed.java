32fa9ab1580f64055903a9f6f0a1cb407d3a4466
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2025-11-20 19:59:45 +0100

    implemented TransformationFixed effect

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/datatypes/effects/TransformationFixed.java
@@ -0,0 +1,48 @@
+package sk.uniba.fmph.dcs.terra_futura.datatypes.effects;
+
+import sk.uniba.fmph.dcs.terra_futura.enums.Resource;
+import sk.uniba.fmph.dcs.terra_futura.interfaces.Effect;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class TransformationFixed implements Effect {
+
+    private final List<Resource> from;
+    private final List<Resource> to;
+    private final int pollution;
+
+    public TransformationFixed(final List<Resource> from, final List<Resource> to, final int pollution) {
+        this.from = from;
+        this.to = to;
+        this.pollution = pollution;
+    }
+
+    @Override
+    public boolean check(final List<Resource> input, final List<Resource> output, final int pollution) {
+        for (Resource r : input) {
+            if (Collections.frequency(from, r) > Collections.frequency(input, r)) {
+                return false;
+            }
+        }
+        for (Resource r : output) {
+            if (Collections.frequency(to, r) != Collections.frequency(output, r)) {
+                return false;
+            }
+        }
+        return this.pollution == pollution;
+
+
+    }
+
+    @Override
+    public boolean hasAssistance() {
+        return false;
+    }
+
+    @Override
+    public String state() {
+        return null;
+    }
+
+}
