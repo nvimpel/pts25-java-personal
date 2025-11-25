0bb015c0e1186f5f1a67930e9ed653425907562c
Matus <matusfilo987@gmail.com>
2025-11-25 20:16:22 +0100

    gameObserver and unit tests

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/GameObserver.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/GameObserver.java
@@ -5,6 +5,30 @@ import sk.uniba.fmph.dcs.terra_futura.interfaces.TerraFuturaObserverInterface;
 class GameObserver {
     java.util.Map<Integer, TerraFuturaObserverInterface> observers;
 
+    GameObserver() {
+        observers = new java.util.HashMap<>();
+    }
+
+    void addObserver(TerraFuturaObserverInterface observer, Integer id) {
+        if(observers.get(id) != null) {
+            throw new IllegalArgumentException("Observer with id " + id + " already exists!");
+        }
+        observers.put(id, observer);
+    }
+
+    void removeObserver(int id) {
+        if(observers.get(id) == null) {
+            throw new IllegalArgumentException("Observer with id " + id + " does not exist!");
+        }
+        observers.remove(id);
+    }
+
     void notifyAllNewState(java.util.Map<Integer, String> state) {
+
+        for(Integer key : state.keySet()) {
+            if(observers.get(key) != null) {
+                observers.get(key).notify(state.get(key));
+            }
+        }
     }
 }
