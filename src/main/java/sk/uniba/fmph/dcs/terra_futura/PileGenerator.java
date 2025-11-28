8e23cbf375e4cc916acec043121efc7fdc7aaa05
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2025-11-28 01:55:50 +0100

    PileGenerator Lint changes

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/PileGenerator.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/PileGenerator.java
@@ -6,23 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Generator of piles defined like this </br>
+ * Generator of piles defined like this.
  * PILE 1:
  * 4x cards generating money
  * 6x cards generating green
  * 6x cards generating red
  * 5x cards generating
  * 2x two material transforming cards
- *
  * PILE 2:
  *
  **/
-public class PileGenerator {
-    public static List<Card> pileGenerator(Deck deck){
+final class PileGenerator {
+    private PileGenerator() {
+        throw new AssertionError("PileGenerator instances forbidden!");
+    }
+    public static List<Card> pileGenerator(final Deck deck) {
         return deck1();
     }
 
-    private static List<Card> deck1(){
+    private static List<Card> deck1() {
         return new ArrayList<>();
     }
 }
