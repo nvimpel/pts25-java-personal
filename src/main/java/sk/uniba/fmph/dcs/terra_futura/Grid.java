4145f6c606e71fb3d14122c0ae3a4c014b8c9873
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2025-11-29 23:39:33 +0100

    Grid finalized

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Grid.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Grid.java
@@ -1,19 +1,20 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
 import sk.uniba.fmph.dcs.terra_futura.datatypes.GridPosition;
-import sk.uniba.fmph.dcs.terra_futura.enums.Deck;
 
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Optional;
 
-public class Grid {
+
+public final class Grid {
     private final ArrayList<ArrayList<Card>> grid = new ArrayList<>();
-    private final static int MAXIMAL_GRID_LENGTH = 5;
-    private int left_bound;
-    private int top_bound;
-    private int right_bound;
-    private int bottom_bound;
+    private static final int MAXIMAL_GRID_LENGTH = 5;
+    private static final int GRID_OFFSET = 2;
+    private final Set<GridPosition> activable = new HashSet<>();
+    private final Bounds bounds = new Bounds();
 
     public Grid() {
         for (int i = 0; i < MAXIMAL_GRID_LENGTH; i++) {
@@ -25,34 +26,96 @@ public class Grid {
     }
 
 
-    public Optional<Card> getCard(GridPosition coordinate) {
-        if (grid.get(coordinate.x() + 2).get(coordinate.y() + 2) == null) {
+    public Optional<Card> getCard(final GridPosition coordinate) {
+        if (grid.get(coordinate.x() + GRID_OFFSET).get(coordinate.y() + GRID_OFFSET) == null) {
             return Optional.empty();
+        } else {
+            return Optional.of(grid.get(coordinate.x() + 2).get(coordinate.y() + 2));
         }
-        else return Optional.of(grid.get(coordinate.x() + 2).get(coordinate.y() + 2));
     }
 
-    public boolean canPutCard(GridPosition coordinate) {
-        return false;
+    public boolean canPutCard(final GridPosition coordinate) {
+
+        if (coordinate.x() > bounds.bottomBound + 2 || coordinate.x() < bounds.topBound - 2) {
+            return false;
+        }
+        if (coordinate.y() > bounds.leftBound + 2 || coordinate.y() < bounds.rightBound - 2) {
+            return false;
+        }
+        //Kontrola obsadenia pozicie
+        return getCard(coordinate).isEmpty();
     }
 
-    public void output(GridPosition coordinate, Card card) {
+    public void putCard(final GridPosition coordinate, final Card card) {
+        if (!canPutCard(coordinate)) {
+            throw new IllegalArgumentException("Cannot put a card there");
+        }
+        if (card == null) {
+            throw new NullPointerException("Cannot put a null card");
+        }
+
+
+        //add to activable
+        activable.add(coordinate);
+        //update to new grid size
+        updateBounds(coordinate);
+        //add card to grid
+        grid.get(coordinate.x() + GRID_OFFSET).set(coordinate.y() + GRID_OFFSET, card);
+        //set activable to all card in same row and column
+        setActivableNeighbours(coordinate);
+
     }
 
-    public boolean canReActivated(GridPosition coordinate) {
-        return false;
+    private void setActivableNeighbours(final GridPosition coordinate) {
+        for (int i = bounds.bottomBound; i <= bounds.topBound; i++) {
+            activable.add(new GridPosition(i, coordinate.y()));
+        }
+        for (int i = bounds.leftBound; i <= bounds.rightBound; i++) {
+            activable.add(new GridPosition(coordinate.x(), i));
+        }
     }
 
-    public void setActivated(GridPosition coordinate) {
+    public boolean canBeActivated(final GridPosition coordinate) {
+        return activable.contains(coordinate);
     }
 
-    public void setActivationPattern(List<GridPosition> pattern) {
+    public void setActivated(final GridPosition coordinate) {
+        activable.remove(coordinate);
+    }
+
+    public void setActivationPattern(final List<GridPosition> pattern) {
+        for (GridPosition coordinate : pattern) {
+            activable.add(new GridPosition(coordinate.x(), coordinate.y()));
+        }
     }
 
     public void endTurn() {
+        activable.clear();
     }
 
     public String state() {
         return "";
+    }
+
+    private void updateBounds(final GridPosition coordinate) {
+        if (coordinate.x() < bounds.bottomBound) {
+            bounds.bottomBound = coordinate.x();
+        }
+        if (coordinate.x() > bounds.topBound) {
+            bounds.topBound = coordinate.x();
+        }
+        if (coordinate.y() < bounds.leftBound) {
+            bounds.leftBound = coordinate.y();
+        }
+        if (coordinate.y() > bounds.rightBound) {
+            bounds.rightBound = coordinate.y();
+        }
+    }
+
+    private static final class Bounds {
+        private int leftBound = 0;
+        private int topBound = 0;
+        private int rightBound = 0;
+        private int bottomBound = 0;
     }
 }
