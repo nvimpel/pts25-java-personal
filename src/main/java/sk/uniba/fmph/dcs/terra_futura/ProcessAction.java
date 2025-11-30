97c77a2866a0d0749cb118b9ff19ecba0bf60cb8
matej <krivosik7@uniba.sk>
2025-11-30 22:41:29 +0100

    ProcessAction - 1.verzia

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/ProcessAction.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/ProcessAction.java
@@ -4,13 +4,80 @@ import sk.uniba.fmph.dcs.terra_futura.enums.Resource;
 import sk.uniba.fmph.dcs.terra_futura.datatypes.GridPosition;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.AbstractMap.SimpleEntry;
 
-public class ProcessAction {
-    static public boolean activateCard(GridPosition cardPosition, Grid grid,
-            List<SimpleEntry<Resource, GridPosition>> inputs,
-            List<Resource> outputs,
-            List<GridPosition> pollution) {
-        return false;
+/**
+ * Overí, či sa daná akcia (aktivácia karty) dá vykonať.
+ * Ak sa niečo nedá vykonať, vráti false.
+ * Bez podpory Assistance.
+ **/
+public final class ProcessAction {
+
+    private ProcessAction() {}
+
+    public static boolean activateCard(
+            final GridPosition cardPosition,
+            final Grid grid,
+            final List<SimpleEntry<Resource, GridPosition>> inputs,
+            final List<Resource> outputs,
+            final List<GridPosition> pollution
+    ) {
+
+        // Overenie, že karta existuje.
+        final Optional<Card> cardOpt = grid.getCard(cardPosition);
+        if (cardOpt.isEmpty()) { return false; }
+        final Card card = cardOpt.get();
+
+        if (!checkInputs(grid, inputs)) { return false; }
+        if (!checkCardEffects(card, inputs, outputs, pollution.size())) { return false; }
+        if (!card.canPutResources(outputs)) { return false; }
+        if (!checkPollution(grid, pollution)) { return false; }
+
+        return true;
+    }
+
+    /** Overenie vstupov. **/
+    private static boolean checkInputs(
+            final Grid grid,
+            final List<SimpleEntry<Resource, GridPosition>> inputs
+    ) {
+        for (final SimpleEntry<Resource, GridPosition> input : inputs) {
+            final Optional<Card> inputCardOpt = grid.getCard(input.getValue());
+            if (inputCardOpt.isEmpty()) { return false; }
+            final Card inputCard = inputCardOpt.get();
+
+            if (!inputCard.canGetResources(List.of(input.getKey()))) return false;
+        }
+        return true;
+    }
+
+    /** Overenie horného a dolného efektu karty. **/
+    private static boolean checkCardEffects(
+            final Card card,
+            final List<SimpleEntry<Resource, GridPosition>> inputs,
+            final List<Resource> outputs,
+            int pollutionCount
+    ) {
+        final List<Resource> inputResources = inputs.stream()
+                                                    .map(SimpleEntry::getKey)
+                                                    .toList();
+        return card.checkUpper(inputResources, outputs, pollutionCount)
+                || card.checkLower(inputResources, outputs, pollutionCount);
+    }
+
+    /** Overenie pollution. **/
+    private static boolean checkPollution(
+            final Grid grid,
+            final List<GridPosition> pollutionPositions
+    ) {
+        for (final GridPosition position : pollutionPositions) {
+            final Optional<Card> pollutionCardOpt = grid.getCard(position);
+            if (pollutionCardOpt.isEmpty()) { return false; }
+            final Card pollutionCard = pollutionCardOpt.get();
+
+            if (!pollutionCard.canPutResources(List.of(Resource.Pollution))) return false;
+        }
+        return true;
     }
 }
